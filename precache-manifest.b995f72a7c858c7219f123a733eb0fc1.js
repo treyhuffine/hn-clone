cd367c7bb6c212f9ad720cc0a7ad877affb4a830
Trey Huffine <treyhuffine@gmail.com>
2018-10-06 15:46:15 -0400

    Updates

---
+++ precache-manifest.b995f72a7c858c7219f123a733eb0fc1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "864cfbe8fdf951ce4691",
+    "url": "/hn-clone/static/js/runtime~main.864cfbe8.js"
+  },
+  {
+    "revision": "f98b20f4886867ab36c3",
+    "url": "/hn-clone/static/js/main.f98b20f4.chunk.js"
+  },
+  {
+    "revision": "35dcbbfb891150eb8640",
+    "url": "/hn-clone/static/js/1.35dcbbfb.chunk.js"
+  },
+  {
+    "revision": "5e01bde3b2eb5f7d6a9c36be02381a04",
+    "url": "/hn-clone/index.html"
+  }
+];
